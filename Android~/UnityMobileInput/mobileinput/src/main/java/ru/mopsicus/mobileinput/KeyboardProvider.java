194bbba0fb432ab741031d134fee90de7cc3170a
Ben Fawcett <ben.fawcett@greatdetail.com>
2022-02-05 14:45:38 +0000

    Fixed keyboardHeight issue on Android

--- Android~/UnityMobileInput/mobileinput/src/main/java/ru/mopsicus/mobileinput/KeyboardProvider.java
+++ Android~/UnityMobileInput/mobileinput/src/main/java/ru/mopsicus/mobileinput/KeyboardProvider.java
@@ -6,12 +6,14 @@
 
 package ru.mopsicus.mobileinput;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.graphics.Point;
 import android.graphics.Rect;
 import android.graphics.drawable.ColorDrawable;
+import android.os.Handler;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -75,8 +77,8 @@ public class KeyboardProvider extends PopupWindow {
         return activity.getResources().getConfiguration().orientation;
     }
 
-    private int heightMax;
-    private int navBarHeight;
+    private int heightMax = 0;
+    private int navBarHeight = 0;
 
     // Handler to get keyboard height
     private void handleOnGlobalLayout() {
@@ -86,13 +88,63 @@ public class KeyboardProvider extends PopupWindow {
         if (rect.bottom > heightMax) {
             heightMax = rect.bottom;
         }
+
         int keyboardHeight = heightMax - rect.bottom;
-        if (keyboardHeight > 0)
-        {
+
+
+        // BF - 5/2/22
+        // Took this out!  It works for 'permanentlyOn nav bars', but now I think most modern devices overlay.
+        // Could improve by potentially assessing which type of nav bar is used.  But, for my purposes, this is best for now.
+/*
+        if (keyboardHeight > 0) {
         	keyboardHeight += navBarHeight;
         }
-        int orientation = getScreenOrientation();
-        notifyKeyboardHeight(keyboardHeight, keyboardHeight, orientation);
+*/
+
+        // BF - 5/2/22
+        // Added this log statement
+/*
+        @SuppressLint("DefaultLocale")
+        String message = String.format(
+              "Rect = [%d, %d, %d, %d], HeightMax = %d,KeyboardHeight = %d, NavBarHeight = %d",
+              rect.left, rect.right, rect.top, rect.bottom, heightMax, keyboardHeight, navBarHeight);
+        Log.d("AGS", message);
+*/
+
+        // BF - 5/2/22
+        // This is what was happening on some devices.  Get two events, the first is wrong, the second is right
+        // These almost at the same time 0.025s between each one.
+        // Rect = [0, 1920, 0, 78], HeightMax = 1200, KeyboardHeight = 1194, NavBarHeight = 72, Ori = 2
+        // Rect = [0, 1920, 0, 639], HeightMax = 1200, KeyboardHeight = 633, NavBarHeight = 72, Ori = 2
+
+        // Because of the situation described above, we now wait for a second event, before
+        // calling the listener method.  We do all this in updateKeyboardHeight and call notifyKeyboardHeight()
+        // from there instead of here..
+
+        //notifyKeyboardHeight(keyboardHeight, keyboardHeight, getScreenOrientation());
+        updateKeyboardHeight(keyboardHeight);
+    }
+
+    // BF - 5/2/22
+    // Add this method, and two working vars
+    boolean mWaitForAnotherKeyboardHeight = false;
+    int mKeyboardHeight = 0;
+    private void updateKeyboardHeight(int keyboardHeight)
+    {
+        mKeyboardHeight = keyboardHeight;
+        if(!mWaitForAnotherKeyboardHeight)
+        {
+            mWaitForAnotherKeyboardHeight = true;
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    int orientation = getScreenOrientation();
+                    notifyKeyboardHeight(mKeyboardHeight, mKeyboardHeight, orientation);
+                    mWaitForAnotherKeyboardHeight = false;
+                }
+            }, 100);
+        }
+
     }
 
     private int getNavigationBarHeight() {
