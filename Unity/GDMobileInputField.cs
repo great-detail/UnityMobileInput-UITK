a190437c6fbb87132ff9cfa354b48493b0e4c0f8
will-gd <will.dye@greatdetail.com>
2026-02-12 12:04:19 +0000

    Fix iOS issues

--- Unity/GDMobileInputField.cs
+++ Unity/GDMobileInputField.cs
@@ -440,14 +440,30 @@ namespace Mopsicus.AG.Modified
             mConfig.Placeholder = "";
             mConfig.PlaceholderColor = Color.clear;
             mConfig.CharacterLimit = mInputObject.maxLength;
-            mConfig.FontSize = mPlaceHolderText.resolvedStyle.fontSize - 12.5f;
+            
+            Rect rect = GetScreenRectFromVisualElement(mInputObject);
+            float ratio = rect.height / mInputObject.resolvedStyle.height;
+            mConfig.FontSize = mInputObject.resolvedStyle.fontSize * ratio;
+
             mConfig.TextColor = mInputObject.resolvedStyle.color;
             mConfig.Align = mInputObject.resolvedStyle.unityTextAlign.ToString();
-            mConfig.ContentType = "Standard";
+            mConfig.InputType = mInputObject.isPasswordField ? "Password" : "Standard";
+            mConfig.ContentType = mInputObject.isPasswordField
+                ? "Password"
+                : mInputObject.keyboardType switch
+                {
+                    TouchScreenKeyboardType.Default => "Standard",
+                    TouchScreenKeyboardType.EmailAddress => "EmailAddress",
+                    TouchScreenKeyboardType.NumberPad => "IntegerNumber",
+                    TouchScreenKeyboardType.PhonePad => "Pin",
+                    TouchScreenKeyboardType.NumbersAndPunctuation => "DecimalNumber",
+                    TouchScreenKeyboardType.NamePhonePad => "Name",
+                    _ => "Standard"
+                };
             mConfig.BackgroundColor = mInputObject.resolvedStyle.backgroundColor;
             mConfig.Multiline = mInputObject.multiline;
             mConfig.KeyboardType = mInputObject.keyboardType.ToString();
-            mConfig.InputType = "Standard";
+            mConfig.InputType = mInputObject.isPasswordField ? "Password" : "Standard";
         }
 
         /// <summary>
@@ -628,7 +644,7 @@ namespace Mopsicus.AG.Modified
         public void SetRectNative()
         {
             Rect rect = GetScreenRectFromVisualElement(mUnityTextInput);
-            if (mLastRect == rect)
+            if (RectApproximatelyEqual(mLastRect, rect))
             {
                 return;
             }
@@ -969,6 +985,17 @@ namespace Mopsicus.AG.Modified
         private string InvariantCultureString(float value)
         {
             return value.ToString("G", System.Globalization.CultureInfo.InvariantCulture);
+        }
+
+        /// <summary>
+        /// Compare two Rects with a small tolerance to avoid floating-point jitter
+        /// </summary>
+        private bool RectApproximatelyEqual(Rect a, Rect b, float tolerance = 1.0f)
+        {
+            return Mathf.Abs(a.x - b.x) < tolerance &&
+                   Mathf.Abs(a.y - b.y) < tolerance &&
+                   Mathf.Abs(a.width - b.width) < tolerance &&
+                   Mathf.Abs(a.height - b.height) < tolerance;
         }
     }
 }
